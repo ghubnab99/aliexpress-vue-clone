7d83caf332e91bc5456e765ce37e8691d177c5e0
mnabil <ebizcom99@gmail.com>
2023-07-06 21:03:37 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  pages: true,
+  modules: [
+    'nuxt-icon',
+    'nuxt-lodash',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/supabase'
+  ],
+  runtimeConfig: {
+    public: {
+      stripePk: process.env.STRIPE_PK_KEY
+    }
+  },
+  alias: {
+    'class-validator': 'class-validator/cjs/index.js',
+  },
+  app: {
+    head: {
+      script: [
+        { src: 'https://js.stripe.com/v3/', defer: true }
+      ],
+    }
+  }
+})
---
+++ pages/success.vue
@@ -0,0 +1,37 @@
+<template>
+    <MainLayout>
+        <div id="SuccessPage" class="mt-4 max-w-[1200px] mx-auto px-2 min-h-[50vh]">
+            <div class="bg-white w-full p-6 min-h-[150px]">
+                <div class="flex items-center text-xl">
+                    <Icon name="clarity:success-standard-line" color="#5FCB04" size="35"/>
+                    <span class="pl-4">Order Successful</span>
+                </div>
+                <p class="text-sm pl-[50px]">Thank you! We've received your order.</p>
+                <p class="text-sm pl-[50px]">
+                    You can view all your orders<NuxtLink 
+                            to="/orders"
+                            class="
+                                w-full 
+                                text-[#FD374F] 
+                                font-semibold 
+                                p-1.5 
+                                rounded-full
+                                mt-4
+                            "
+                        >here
+                        </NuxtLink>
+                </p>
+            </div>
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore()
+
+onMounted(() => {
+    setTimeout(() => userStore.isLoading = false, 300)
+})
+</script>
